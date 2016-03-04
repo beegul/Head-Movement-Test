1d78532a0cc64bd8e6e2765163012f8232f94fd3
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-03-04 04:56:42 -0800

    VR view change and camera reset.
    - Changed position of camera to better match real life test setup.
    - Added a reset position button so if the camera goes weird, it can be
    reset.

---
+++ HeadMovementTest/Assets/Scripts/CenterHeadset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterHeadset : MonoBehaviour
+{
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            OVRManager.display.RecenterPose();
+        }        
+    }
+}
